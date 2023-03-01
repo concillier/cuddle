9395c96d0ebfebe647b3fce4c5d45e2a83fd914f
Concillier <concillier.data@gmail.com>
2023-03-01 16:32:42 +0100

    Users can edit the items they themselves added

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk): 
     Item = get_object_or_404(item, pk=pk)
@@ -32,6 +32,27 @@ def new(request):
         'form': form,
         'title': 'New item',
         })
+
+@login_required
+def edit(request, pk):
+    Item = get_object_or_404(item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST': 
+        form = EditItemForm(request.POST, request.FILES, instance=Item)
+
+        if form.is_valid(): 
+            form.save()
+
+            return redirect('item:detail', pk=Item.id)
+        
+    else: 
+            form = EditItemForm(instance=Item)
+
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'Edit item',
+        })
+
 
 @login_required
 def delete(request,pk):

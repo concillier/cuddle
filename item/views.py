79ed813ea2edfcc93ee697a3f2a0401f5da93676
Concillier <concillier.data@gmail.com>
2023-03-09 13:02:27 +0100

    Search functionality added

--- item/views.py
+++ item/views.py
@@ -1,7 +1,33 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import item
+from .models import item, Category
 from .forms import NewItemForm, EditItemForm
+
+# makes searching easier
+from django.db.models import Q
+
+
+def items(request): 
+     
+     query = request.GET.get('query', '')
+     category_id = request.GET.get('category', 0)
+     items = item.objects.filter(is_sold=False)
+     categories = Category.objects.all()
+
+     if category_id: 
+          items = items.filter(category_id = category_id)
+     
+
+     if query: 
+          items = items.filter(Q( name__icontains = query)| Q(description__icontains = query))
+
+     return render(request, 'items.html', {
+          'items': items,
+          'title': 'Browse Items', 
+          'query': query,
+          'categories': categories,
+          'category_id': int(category_id),
+     })
 
 def detail(request, pk): 
     Item = get_object_or_404(item, pk=pk)
